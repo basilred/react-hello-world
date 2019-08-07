83b5ea03e0918e127733c3b8f48f959c65507079
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-08 00:05:58 +0300

    Inline If with Logical && Operator

--- src/index.js
+++ src/index.js
@@ -134,8 +134,22 @@ class LoginControl extends React.Component {
   }
 }
 
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>You have {unreadMessages.length} unread messages.</h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
 ReactDOM.render(
-  <LoginControl />,
+  <Mailbox unreadMessages={messages} />,
   document.getElementById('root')
 );
 
