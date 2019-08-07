c776623a8a435ee3dcb53861f00bee2e7d4ba226
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-07 12:28:23 +0300

    Add event handler

--- src/index.js
+++ src/index.js
@@ -46,8 +46,29 @@ function App() {
   );
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+  }
+
+  handleClick = () => {
+    this.setState((state) => ({
+      isToggleOn: !state.isToggleOn,
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );
 
