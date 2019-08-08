52fa68574ee1c2f130efb4b93fe028b279044558
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-08 23:56:13 +0300

    Create two lists

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Page from './chapters1-7';
+import ListsKeys from './ListsKeys';
+
+const tree = (
+  <div>
+    <Page />
+    <ListsKeys.List />
+    <ListsKeys.NumberList numbers={ListsKeys.numbers} />
+  </div>
+);
 
 ReactDOM.render(
-  <Page />,
+  tree,
   document.getElementById('root')
 );
