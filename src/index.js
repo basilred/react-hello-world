b97390201b171ea7fc28786ac6257561fda65751
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-09 00:29:31 +0300

    Create unique keys among siblings

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Page from './chapters1-7';
-import { NumberList, numbers } from './ListsKeys';
+import { NumberList, numbers, Blog, posts } from './ListsKeys';
 
 const tree = (
   <div>
     <Page />
     <NumberList numbers={numbers} />
+    <Blog posts={posts} />
   </div>
 );
 
