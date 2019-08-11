5b288860ddebcc032bf3343fbb0a3a0e74b9ab4d
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-11 23:25:36 +0300

    Add specialization

--- src/CompositionVsInheritance.js
+++ src/CompositionVsInheritance.js
@@ -8,17 +8,58 @@ function FancyBorder(props) {
   );
 }
 
-function WelcomeDialog() {
+function Dialog(props) {
   return (
     <FancyBorder color="blue">
       <h1 className="Dialog-title">
-        Welcome
+        {props.title}
       </h1>
       <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
+        {props.message}
       </p>
+      {props.children}
     </FancyBorder>
   );
+}
+
+function WelcomeDialog() {
+  return (
+    <Dialog
+      title="Welcome"
+      message="Thank you for visiting our spacecraft!" />
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+
+  render() {
+    return (
+      <Dialog
+        title="Mars Exploration Program"
+        message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
 }
 
 function SplitPane(props) {
@@ -43,4 +84,4 @@ function App() {
   );
 }
 
-export { WelcomeDialog, App };
+export { WelcomeDialog, App, SignUpDialog };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Page from './chapters1-7';
 import { NumberList, numbers, Blog, posts } from './ListsKeys';
 import { NameForm, EssayForm, FlavorForm, Reservation } from './Forms';
 import Calculator from './LiftingStateUp';
-import { WelcomeDialog, App } from './CompositionVsInheritance';
+import { WelcomeDialog, App, SignUpDialog } from './CompositionVsInheritance';
 
 const tree = (
   <div>
@@ -20,6 +20,7 @@ const tree = (
     <Calculator />
     <WelcomeDialog />
     <App />
+    <SignUpDialog />
   </div>
 );
 
