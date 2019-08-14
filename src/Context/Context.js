b5fee93938cac87e127a59bfd531abad31e3b5a5
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-14 10:05:21 +0300

    Pass theme value using Context

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,27 +1,44 @@
 import React from 'react';
 
+// Контекст позволяет передавать значение глубоко
+// в дерево компонентов без явной передачи пропсов
+// на каждом уровне. Создадим контекст для текущей
+// UI-темы (со значением "light" по умолчанию).
+const ThemeContext = React.createContext('light');
+
 class App extends React.Component {
   render() {
-    return <Toolbar theme="dark" />;
+    // Компонент Provider используется для передачи текущей
+    // UI-темы вниз по дереву. Любой компонент может использовать
+    // этот контекст и не важно, как глубоко он находится.
+    // В этом примере мы передаём "dark" в качестве значения контекста.
+    return (
+      <ThemeContext.Provider value="dark">
+        <Toolbar />
+      </ThemeContext.Provider>
+    );
   }
 }
 
-function Toolbar(props) {
-  // Компонент Toolbar должен передать проп "theme" ниже,
-  // фактически не используя его. Учитывая, что у вас в приложении
-  // могут быть десятки компонентов, использующих UI-тему,
-  // вам придётся передавать проп "theme" через все компоненты.
-  // И в какой-то момент это станет большой проблемой.
+// Компонент, который находится в середине,
+// теперь не должен явно передавать UI-тему вниз.
+function Toolbar() {
   return (
     <div>
-      <ThemeButton theme={props.theme} />
+      <ThemeButton />
     </div>
   );
 }
 
 class ThemeButton extends React.Component {
+  // Определяем contextType, чтобы получить значение контекста.
+  // React найдёт (выше по дереву) ближайший Provider-компонент,
+  // предоставляющий этот контекст, и использует его значение.
+  // В этом примере значение UI-темы будет "dark".
+  static contextType = ThemeContext;
+
   render() {
-    return <button theme={this.props.theme} />;
+    return <button theme={this.context}>{this.context}</button>;
   }
 }
 
