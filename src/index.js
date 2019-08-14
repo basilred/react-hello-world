179e143cd2a7ccb885b520bfc9935a09a5a3cf0b
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-14 09:51:49 +0300

    Intro to the Context

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function ListItem({ item }) {
-  return (
-    <Fragment>
-      <dt>{item.term}</dt>
-      <dd>{item.description}</dd>
-    </Fragment>
-  );
-}
-
-function Glossary(props) {
-  return (
-    <dl>
-      {props.items.map(item => (
-        <ListItem item={item} key={item.id} />
-      ))}
-    </dl>
-  );
-}
-
-const items = [
-  { id: 1, term: 'iphone', description: 'mobile phone' },
-  { id: 2, term: 'apple', description: 'fruit' },
-];
+import App from './Context/Context';
 
 ReactDOM.render(
-  <Glossary items={items}/>,
+  <App />,
   document.getElementById('root')
 );
