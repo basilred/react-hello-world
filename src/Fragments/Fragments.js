e75afb952210d828093edd5e08110d9aacf557c1
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-18 23:03:48 +0300

    Use Fragments short syntax

--- src/Fragments/Fragments.js
+++ src/Fragments/Fragments.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default class Columns extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <td>Hello</td>
         <td>World</td>
-      </React.Fragment>
+      </>
     );
   }
 }
